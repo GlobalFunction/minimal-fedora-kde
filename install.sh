60677e191c1b34d77991463ca9072e3e05a3d43e
GlobalFunction <33298464+GlobalFunction@users.noreply.github.com>
2020-11-29 16:39:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,32 +10,26 @@ fi
 # RPM FUSION Section https://rpmfusion.org/Configuration
 # ==================================================================
 
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+dnf copr enable taw/joplin
 
 
 # ==================================================================
 # KDE Base Install
 # ==================================================================
 
-dnf install \
+dnf install -y \
   @"KDE Plasma Workspaces" \
   ark \
   gscan2pdf \
   libreoffice \
   vlc \
   kde-connect \
-  firefox
-
-# Enable kde boot
-systemctl enable sddm
-systemctl set-default graphical.target
-
-
-# ==================================================================
-# Applications
-# ==================================================================
-
-dnf install \
+  firefox \
   freecad \
   keepassxc \
   texmaker \
@@ -52,28 +46,13 @@ dnf install \
   rclone-browser \
   kdenlive \
   neofetch \
-  rdfind
+  rdfind \
+  akmod-nvidia \
+  xorg-x11-drv-nvidia-cuda \
+  nvidia-settings \
+  code \
+  joplin
 
-
-# ==================================================================
-# Nvidia Drivers from RPM Fusion
-# ==================================================================
-
-dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda nvidia-settings
-
-
-# ==================================================================
-# VS Code Repo
-# ==================================================================
-
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf install code
-
-
-# ==================================================================
-# joplin repo
-# ==================================================================
-
-dnf copr enable taw/joplin
-dnf install joplin
+# Enable kde boot
+systemctl enable sddm
+systemctl set-default graphical.target
